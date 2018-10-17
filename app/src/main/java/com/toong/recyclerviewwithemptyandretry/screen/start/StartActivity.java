6a8aff10f9c031a0e5d440ace04d50bf9a1439f4
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-10-17 14:50:08 +0700

    [Improvement] Add Start activity for navigation

---
+++ app/src/main/java/com/toong/recyclerviewwithemptyandretry/screen/start/StartActivity.java
@@ -0,0 +1,37 @@
+package com.toong.recyclerviewwithemptyandretry.screen.start;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.toong.recyclerviewwithemptyandretry.R;
+import com.toong.recyclerviewwithemptyandretry.screen.book.BookActivity;
+import com.toong.recyclerviewwithemptyandretry.screen.user.UserActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        findViewById(R.id.button_simple_no_loadmore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(BookActivity.class);
+            }
+        });
+
+        findViewById(R.id.btn_1type_loadmore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(UserActivity.class);
+            }
+        });
+    }
+
+    private void startActivity(Class<? extends Activity> activity) {
+        startActivity(new Intent(this, activity));
+    }
+}
